fd52c9cca1a0b3894588c95fe5cc3ad1aa32b4cd
Liam Owen <liam@polygami.co.uk>
2018-02-06 12:48:45 +0100

    create User model, configure passport

--- README.md
+++ README.md
@@ -34,3 +34,15 @@
 HTTP is supposed to be a **stateless protocol**, meaning that when a request is sent, those requests are one time transactions and don't contain data such as history, which make it hard to do things like user authentication because we want users to be able to stay logged in. We do this by using **sessions**.
 
 Sessions are a way to provide a **state**, so when every request is made, there is some information that is saved.
+
+----------
+### Auth Code Along part 1
+* Set up folder structure
+* Install needed packages
+* Add root route and template
+* Add secret route and template
+
+----------
+### Auth Code Along part 2
+* Create User model
+* Configure passport
--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-var express		= require("express"),
-	app			= express(),
-	mongoose	= require("mongoose")
+var	express					= require("express"),
+	mongoose				= require("mongoose"),
+	passport				= require("passport"),
+	bodyParser				= require("body-parser"),
+	User					= require("./models/user"),
+	LocalStrategy			= require("passport-local"),
+	passportLocalMongoose	= require("passport-local-mongoose");
+
 
 mongoose.connect("mongodb://localhost/auth_demo");
 
+var app	= express();
 app.set("view engine", "ejs");
+app.use(require("express-session")({
+	secret: "Now are the winters of our discontent.",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // INDEX
 app.get("/", function(req, res) {
